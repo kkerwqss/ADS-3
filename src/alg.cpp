48357f724b96709e7b63b0aea748d411d2047871
kkerwqss <banykinaks@yandex.ru>
2024-03-19 20:23:00 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int count = 0, left = 0, right = size - 1;
+while (left < right) {
+  int mid = (left + right) / 2;
+  if (arr[mid] == value) {
+    count++;
+    int temp = mid - 1;
+    while (temp >= 0 && arr[temp] == value) {
+      count++;
+      temp--;
+    }
+    temp = mid + 1;
+    while (temp < size && arr[temp] == value) {
+      count++;
+      temp++;
+    }
+    return count;
+  } else if (arr[mid] < value) {
+    left = mid + 1;
+  } else {
+    right = mid - 1;
+  }
+}
   return 0; // если ничего не найдено
 }
